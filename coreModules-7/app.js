2bc3091e75ab8f6af15503789339637f55c0078c
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-03-25 23:52:47 +0700

    finish coreModule

--- coreModules-7/app.js
+++ coreModules-7/app.js
@@ -21,5 +21,45 @@ fs.writeFile("data/test.txt", "Hello world, secara Asynchronous", (e) => {
 });
 
 // membaca isi file (synchronous)
-const data = fs.readFileSync("catatan.txt", "utf-8");
-console.log(data);
+// utf-8: menampilkan apa yang dibaca program diubah menjadi string karena default bawaan menggunkan readFileSync akan menampilkan ouput angka buffer
+// const data = fs.readFileSync("catatan.txt", "utf-8");
+// console.log(data);
+
+// membaca isi file (Asynchronous)
+// (e): adalah callback, untuk menampilkan error
+// data: adalah isi dari text file catatan.txt yang awalnya adalah buffer dan sudah diubah menjadi string menggunakan 'utf-8'
+// if: jika terjadi error
+// fs.readFile("catatan.txt", "utf-8", (e, data) => {
+//   if (e) throw e;
+//   console.log(data);
+// });
+
+// Readline
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Masukkan nama anda:", (nama) => {
+  rl.question("Masukkan umur anda: ", (umur) => {
+    // mendapatkan dan membuat object nama dan umur
+    const contact = { nama, umur };
+
+    // membaca isi file pada contact.json yang di encoding menjadi string menggunakan utf-8
+    const file = fs.readFileSync("data/contact.json", "utf-8");
+
+    // mengubah isi file yang tadinya JSON string menjadi object menggunakan parse
+    const contacts = JSON.parse(file);
+
+    // menambahkan objek contact yang baru dibuat ke dalam array contacts yang berasal dari parsing file JSON.
+    contacts.push(contact);
+
+    // menulis data contacts yang di perbaharui ke file json menjadi string representasi JSON
+    fs.writeFileSync("data/contact.json", JSON.stringify(contacts));
+    console.log("TerimaKasih sudah memasukkan data.");
+
+    // untuk menutup jalannya program
+    rl.close();
+  });
+});
