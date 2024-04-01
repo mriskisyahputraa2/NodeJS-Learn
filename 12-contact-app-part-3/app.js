4a447ecb7b52fed5fad59a886221f8b623e1b715
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-04-01 15:40:43 +0700

    update

--- 12-contact-app-part-3/app.js
+++ 12-contact-app-part-3/app.js
@@ -2,36 +2,66 @@ const yargs = require("yargs"); // import module yargs
 const contacts = require("./contacts"); // import module contacts
 
 // mendeklarasi command add
-yargs.command({
-  command: "add", // nama command yang digunakan di terminal
-  describe: "Menambahkan contact baru", // deskripsi singkat pada command
+yargs
+  .command({
+    command: "add", // nama command yang digunakan di terminal
+    describe: "Menambahkan contact baru", // deskripsi singkat pada command
 
-  // mendefinisikan argument yang diterima command add, berupa hasil object
+    // mendefinisikan argument yang diterima command add, berupa hasil object
+    builder: {
+      // argument nama
+      nama: {
+        describe: "Nama lengkap", // deskripsi argument nama
+        demondOption: true, // wajib isi atau tidak, kalo wajib true, kalo tidak false
+        type: "string", // type data argument
+      },
+      // argument email
+      email: {
+        describe: "Email",
+        demondOption: false,
+        type: "string",
+      },
+      // argument noHP
+      noHP: {
+        describe: "No HP",
+        demondOption: true,
+        type: "string",
+      },
+    },
+
+    // handler function, fungsi yang dijalankan ketika command add dipanggil
+    handler(argv) {
+      // yang dijalankan yaitu argument nama, email dan noHP, yang disimpan dalam function simpanContact
+      contacts.simpanContact(argv.nama, argv.email, argv.noHP);
+    },
+  })
+  // untuk menambahkan warning jika hanya melakukan command node app saja
+  .demandCommand();
+
+// menampilkan list nama dan noHP
+yargs.command({
+  command: "list",
+  describe: "Menampilkan semua nama & no HP contact",
+  // tidak menggunakan argument karena tidak menggunakan builder
+  handler() {
+    contacts.listContact();
+  },
+});
+
+// menampilkan detail sebuah kontak
+yargs.command({
+  command: "detail",
+  describe: "Menampilkan detail sebuah contact berdasarkan nama",
   builder: {
-    // argument nama
     nama: {
-      describe: "Nama lengkap", // deskripsi argument nama
-      demondOption: true, // wajib isi atau tidak, kalo wajib true, kalo tidak false
-      type: "string", // type data argument
-    },
-    // argument email
-    email: {
-      describe: "Email",
-      demondOption: false,
-      type: "string",
-    },
-    // argument noHP
-    noHP: {
-      describe: "No HP",
+      describe: "Nama Lengkap",
       demondOption: true,
       type: "string",
     },
   },
-
-  // handler function, fungsi yang dijalankan ketika command add dipanggil
+  // knp ada argument sedangkan command list tidak ada? karena disini kita menggunkan builder, sedangkan di list tidak menggunakan builder
   handler(argv) {
-    // yang dijalankan yaitu argument nama, email dan noHP, yang disimpan dalam function simpanContact
-    contacts.simpanContact(argv.nama, argv.email, argv.noHP);
+    contacts.detailContact(argv.nama);
   },
 });
 
--- 12-contact-app-part-3/contacts.js
+++ 12-contact-app-part-3/contacts.js
@@ -10,6 +10,16 @@ if (!fs.existsSync(dirPath)) {
   fs.mkdirSync(dirPath);
 }
 
+// function isi dari data json
+const loadContact = () => {
+  // membaca isi file pada contact.json yang di encoding menjadi string menggunakan utf-8 untuk memastikan format JSON.
+  const file = fs.readFileSync("data/contact.json", "utf-8");
+
+  // mengubah isi file yang tadinya JSON string menjadi object menggunakan parse
+  const contacts = JSON.parse(file);
+  return contacts;
+};
+
 // membuat file contact.json jika belum ada
 const dataPath = "./data/contact.json";
 // cek jika file contact.json tida ada
@@ -23,11 +33,8 @@ const simpanContact = (nama, email, noHP) => {
   // membuat datanya menjadi object ketika dimasukkan ke dalam json
   const contact = { nama, email, noHP };
 
-  // membaca isi file pada contact.json yang di encoding menjadi string menggunakan utf-8 untuk memastikan format JSON.
-  const file = fs.readFileSync("data/contact.json", "utf-8");
-
-  // mengubah isi file yang tadinya JSON string menjadi object menggunakan parse
-  const contacts = JSON.parse(file);
+  // mengambil data json
+  const contacts = loadContact();
 
   // validasi data nama
   // mengecek apakah nama yang diinputkan sudah ada di database.
@@ -67,4 +74,37 @@ const simpanContact = (nama, email, noHP) => {
   console.log(chalk.green.inverse("TerimaKasih sudah memasukkan data."));
 };
 
-module.exports = { simpanContact };
+// function untuk menampilkan listContact
+const listContact = () => {
+  // mengambil data json
+  const contacts = loadContact();
+
+  console.log(chalk.cyan.inverse("Daftar Kontak: ."));
+
+  // melukakan looping data dalam json
+  contacts.forEach((contact, i) => {
+    console.log(`${i + 1}. Nama: ${contact.nama} - NoHP: ${contact.noHP}`);
+  });
+};
+
+// function detailContact
+const detailContact = (nama) => {
+  // mengambil data json
+  const contacts = loadContact();
+
+  // mencari data json nama, sesuai dengan nama dalam json
+  const contact = contacts.find(
+    // cari nama yang diinputkan sesuai dengan nama dalam json dan diubah menjadi huruf kecil menggunakan toLowerCase
+    (contact) => contact.nama.toLowerCase() === nama.toLowerCase()
+  );
+
+  // jika contact nya tidak ada maka
+  if (!contact) {
+    // tampilkan pesan error tersebut
+    console.log(chalk.red.inverse.bold(` ${nama} tidak ditemukan!`));
+    // berhentikan program
+    return false;
+  }
+};
+
+module.exports = { simpanContact, listContact, detailContact };
