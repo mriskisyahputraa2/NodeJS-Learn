fdc9f71d6f756f89735f1a221b612bbd04a924ee
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-04-19 12:17:15 +0700

    update

--- 13-nodejs-web-server/app.js
+++ 13-nodejs-web-server/app.js
@@ -2,6 +2,18 @@ const http = require("http");
 const fs = require("fs");
 const port = 3000;
 
+const rederHTML = (path, res) => {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.write("file not found");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+};
+
 http
   .createServer((req, res) => {
     res.writeHead(200, {
@@ -10,21 +22,11 @@ http
 
     const url = req.url;
     if (url === "/about") {
-      res.write("<h1>Ini adalah halaman about</h1>");
-      res.end();
+      rederHTML("./about.html", res);
     } else if (url === "/contact") {
-      res.write("<h1>Ini adalah halaman contact</h1>");
-      res.end();
+      rederHTML("./contact.html", res);
     } else {
-      fs.readFile("index.html", (err, data) => {
-        if (err) {
-          res.writeHead(404);
-          res.write("Error: file not found");
-        } else {
-          res.write(data);
-        }
-        res.end();
-      });
+      rederHTML("./index.html", res);
     }
   })
   .listen(port, () => {
